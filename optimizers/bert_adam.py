7a57d3352f418fa0dd941b9ffc93c8128cb4955c
Nghia Hieu Nguyen <nghiangh@uit.edu.vn>
2026-04-19 22:15:09 +0700

    updated the BertAdam

--- optimizers/bert_adam.py
+++ optimizers/bert_adam.py
@@ -21,17 +21,26 @@ import torch
 from torch.optim import Optimizer
 from torch.optim.optimizer import required
 from torch.nn.utils import clip_grad_norm_
+import torch.distributed as dist
 #from fused_adam_local import FusedAdam
 from apex.optimizers import FusedAdam
 from apex.multi_tensor_apply import multi_tensor_applier
 # import amp_C
-from utils import is_main_process
 
 # multi_tensor_l2norm = amp_C.multi_tensor_l2norm
 # lamb_compute_update = amp_C.multi_tensor_lamb_stage1_cuda
 # lamb_apply_update = amp_C.multi_tensor_lamb_stage2_cuda
 # scale = amp_C.multi_tensor_scale
 
+def get_rank():
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+    return dist.get_rank()
+
+def is_main_process():
+    return get_rank() == 0
 
 def warmup_cosine(x, warmup=0.002):
     if x < warmup:
