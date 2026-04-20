b54ab96ba1163d8b08868a5dc238fcd14d94c0d6
BinhAnndapoet <dinhlebinhan2005@gmail.com>
2026-04-20 13:26:03 +0700

    add -100 mask

--- builders/dataset_builder.py
+++ builders/dataset_builder.py
@@ -127,8 +127,6 @@ class SubsetDataset(Dataset):
         
         # Apply masking (mask token id is 0)
         input_ids[mask_indices] = 0
-
-        labels[~mask_indices] = -100
         
         return {
             'input_ids': input_ids,
@@ -222,6 +220,8 @@ class PretrainingDataset(Dataset):
         
         # Apply masking (mask token id is 0)
         input_ids[mask_indices] = 0
+
+        labels[~mask_indices] = -100
         
         return {
             'input_ids': input_ids,
